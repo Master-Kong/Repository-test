e482cfe45f2f726a9a344a0d21a2b8f185f4d3b9
masterKong <000@qq.com>
2020-03-20 13:22:37 +0800

    添加注释

--- ideawork3/test1/src/testRepository/Hellow1.java
+++ ideawork3/test1/src/testRepository/Hellow1.java
@@ -3,7 +3,8 @@ package testRepository;
 public class Hellow1 {
     public static void main(String[] args) {
         System.out.println("你好!");
-        System.out.println("疫情结束了!");
+        System.out.println("疫情结束了!");   //添加注释
+
 
     }
 }
