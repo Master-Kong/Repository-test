6078a89f3e20eb6fabbde955eaf0ea4c03295cc6
masterKong <000@qq.com>
2020-03-20 13:21:29 +0800

    修改一行代码

--- ideawork3/test1/src/testRepository/Hellow1.java
+++ ideawork3/test1/src/testRepository/Hellow1.java
@@ -4,7 +4,6 @@ public class Hellow1 {
     public static void main(String[] args) {
         System.out.println("你好!");
         System.out.println("疫情结束了!");
-        System.out.println("111");
 
     }
 }
